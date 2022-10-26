08c9d864e7a10113e847f437db61982017a9d68c
Honey <boinaavinashkumar@gmail.com>
2022-10-26 12:06:29 +0530

    suresh version 1

--- src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -2,12 +2,12 @@ package ETA_SB.jntu_calc_application;
 
 public class Calculator {
 	public double doAdd(double num1, double num2) {
-		return num1 + num2;
+		return num1 + num2; //addition
 	}
 
 	public double doSub(double num1, double num2) {
 		//comment line
-		return num1 - num2;
+		return num1 - num2; //subtraction
 	}
 
 	public double doMul(double num1, double num2) {
@@ -15,7 +15,8 @@ public class Calculator {
 	}
 
 	public double doDiv(double num1, double num2) {
-		return num1 / num2;
+		return num1 / num2; // division
 	}
+	
 
 }
